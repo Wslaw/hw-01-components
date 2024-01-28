91087102a056b12c46f276d44915d37b78003e97
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-01-28 22:02:38 +0200

    UpdateSingle

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,53 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+// import Profile from "./Profile/Profile";
+// import user from '../data/user.json';
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       <Profile
+//         username={user.username}
+//         tag={user.tag}
+//         location={user.location}
+//         avatar={user.avatar}
+//         stats = {user.stats}
+//       />
+//     </div>
+//   );
+// };
+
+
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import style from './Modal.module.css';
+
+const modalRoot = document.getElementById('modal-root');
+const Modal = ({ close, image }) => {
+  const closeModal = ({ target, currentTarget, code }) => {
+    if (target === currentTarget || code === 'Escape') {
+      close();
+    }
+  };
+  useEffect(() => {
+    document.addEventListener('keydown', closeModal);
+    return () => document.removeEventListener('keydown', closeModal);
+  });
+  
+  return createPortal(
+    <div onClick={closeModal} className={style.overlay}>
+      <div className={style.modal}>
+        <img className={style.img} src={image.largeImageURL} alt="" />
+      </div>
+    </div>,
+    modalRoot
   );
 };
+export default Modal;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,40 @@
+// import { Component } from "react";
+import React from "react";
+import styles from './profile.module.css'
+
+const Profile = ({ username, tag, description, location, avatar, stats }) => {
+    
+    console.log(stats);
+
+        return (
+          <div className={styles.profile}>
+            <div className={styles.description}>
+              <img
+                src={avatar}
+                alt={description}
+                className={styles.avatar}
+              />
+                    <p className={styles.name}>{username}</p>
+                    <p className={styles.tag}>{tag }</p>
+                    <p className={styles.location}>{location }</p>
+            </div>
+
+            <ul className={styles.stats}>
+              <li>
+                <span className={styles.label}>Followers</span>
+                <span className={styles.quantity}>1000</span>
+              </li>
+              <li>
+                <span className={styles.label}>Views</span>
+                <span className={styles.quantity}>2000</span>
+              </li>
+              <li>
+                <span className={styles.label}>Likes</span>
+                <span className={styles.quantity}>3000</span>
+              </li>
+            </ul>
+          </div>
+        );
+    }
+
+export default Profile;
